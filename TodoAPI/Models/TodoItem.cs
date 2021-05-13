5dbcf02e1bba7b77998390053737d4ae47514ddd
Aditya Oberai <adityaoberai1@gmail.com>
2021-05-13 18:50:14 +0530

    Mapped Json for models

--- TodoAPI/Models/TodoItem.cs
+++ TodoAPI/Models/TodoItem.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TodoAPI.Models
 {
     public class TodoItem
     {
+        [JsonPropertyName("id")]
         public long Id { get; set; }
+        [JsonPropertyName("task")]
         public string TaskToDo { get; set; }
     }
 }
--- TodoAPI/Models/User.cs
+++ TodoAPI/Models/User.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TodoAPI.Models
 {
     public class User
     {
+        [JsonPropertyName("fname")]
         public string FirstName { get; set; } = "";
+        [JsonPropertyName("lname")]
         public string LastName { get; set; } = "";
-        public string Email { get; set; } = "";
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+        [JsonPropertyName("isActive")]
         public bool IsActive { get; set; } = false;
+        [JsonPropertyName("role")]
         public string Role { get; set; } = "User";
+        [JsonPropertyName("password")]
         public string Password { get; set; }
+        [JsonPropertyName("token")]
         public string Token { get; set; } = "";
     }
 }
